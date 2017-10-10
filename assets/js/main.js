adb3bce03754e815f35bc91b436b2e88b09821e4
erik-kert <erik0kert@gmail.com>
2017-10-10 16:45:36 +0100

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,7 @@
                 container: 'map',
                     style: 'mapbox://styles/mapbox/streets-v9'
                     
+                    
                 });
                 
                  
